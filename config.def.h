255b50061e77f403f5f02ce9b60ed4dd594feb27
Rmlyy <hello@rmly.dev>
2022-01-03 20:59:10 +0200

    change color scheme & font

--- config.def.h
+++ config.def.h
@@ -12,12 +12,12 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code Regular:size=17" };
+static const char *fonts[]          = { "Noto Sans:size=17" };
 static const char col_gray1[]       = "#1c1a1a";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#d6d4d4";
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#1175c2";
+static const char col_cyan[]        = "#f3e5ab";
 static const char *colors[][3]      = {
 	/*               fg         bg              border   */
 	[SchemeNorm] = { col_gray3, col_gray1,      col_gray2 },
@@ -25,7 +25,7 @@ static const char *colors[][3]      = {
 	[SchemeStatus]  = { col_gray3, col_gray1,   "#000000" }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]  = { col_gray4, col_gray1,  "#000000" }, // Tagbar left selected {text,background,not used but cannot be empty}
         [SchemeTagsNorm]  = { col_gray3, col_gray1, "#000000" }, // Tagbar left unselected {text,background,not used but cannot be empty}
-        [SchemeInfoSel]  = { "#ffffff", col_cyan,   "#000000" }, // infobar middle  selected {text,background,not used but cannot be empty}
+        [SchemeInfoSel]  = { "#000000", col_cyan,   "#000000" }, // infobar middle  selected {text,background,not used but cannot be empty}
         [SchemeInfoNorm]  = { col_gray3, col_gray1, "#000000" }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
